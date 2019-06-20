f595042015a07820b85867b4ee6c1931f5bb9c94
osdal <osdal67@gmail.com>
2019-06-20 16:24:42 +0300

    Блок с лаботами (начало)

---
+++ gulp/config.js
@@ -0,0 +1,13 @@
+module.exports = [
+    './gulp/tasks/sass.js',
+    './gulp/tasks/scripts.js',
+    './gulp/tasks/copy.js',
+    './gulp/tasks/clean.js',
+    './gulp/tasks/allimg.js',
+    './gulp/tasks/html.js',
+    './gulp/tasks/images.js',
+    './gulp/tasks/serve.js',
+    './gulp/tasks/webp.js',
+    './gulp/tasks/svg.js',
+    './gulp/tasks/build.js',
+];
